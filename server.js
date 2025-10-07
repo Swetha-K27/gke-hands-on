035eaac5b17683bde9124521271c4a8087a87071
Swetha-K12 <swetharkmy@gmail.com>
2025-10-07 18:48:52 +0000

    Initial project setup with CI/CD pipeline2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-  res.send(`Hello from webapp2! version: ${process.env.VERSION || 'v0.1'}`);
+  res.send(`Hellojijue from webapp2! version: ${process.env.VERSION || 'v0.1'}`);
 });
 app.get('/health', (req, res) => res.status(200).send('OK'));
 app.listen(port, () => console.log(`Listening on ${port}`));
